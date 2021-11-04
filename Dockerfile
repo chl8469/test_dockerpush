c8f409a070c5aa7af67cc4228ade7cec6e275adf
hlchoi <hl8469@gmail.com>
2021-11-04 05:25:31 +0000

    split branch

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM python:3.8
 
-COPY . /
-
+COPY ./install_pkgs.sh /install_pkgs.sh
 RUN /install_pkgs.sh
+
+COPY . /
 
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--workers", "3"]
--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-from dotenv import load_dotenv
-from google.cloud import storage
 from fastapi import FastAPI
-import io
-import pandas as pd
-
-load_dotenv()
 
 app = FastAPI()
 
-storage_client = storage.Client()
-bucket = storage_client.bucket("choonsik-storage")
-
-
 @app.get("/")
 async def root():
-    blob = bucket.blob("data/insurance.csv")
-    bytes = blob.download_as_bytes()
-    file_buffer = io.BytesIO(bytes)
-    df = pd.read_csv(file_buffer)
-    # print(df)
-    return {"message": df.values.tolist()[:3]}
+    return {"message": "hello world"}
